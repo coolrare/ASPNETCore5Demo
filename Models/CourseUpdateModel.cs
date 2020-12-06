f3e96edd21bb25687cbd600632e9b59383c49d52
Will <doggy.huang@gmail.com>
2020-12-07 02:02:27 +0800

    示範 [ModelMetadataType] 用法 (在程式碼產生器的檔案外新增驗證屬性)

--- Models/CourseUpdateModel.cs
+++ Models/CourseUpdateModel.cs
@@ -1,10 +1,19 @@
 
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace ASPNETCore5Demo.Models
 {
+    [ModelMetadataType(typeof(CourseUpdateModelMetadata))]
     public class CourseUpdateModel
+    {
+        public string Title { get; set; }
+
+        public int Credits { get; set; }
+    }
+
+    internal class CourseUpdateModelMetadata
     {
         [Required]
         [StringLength(30, ErrorMessage = "Title 太長")]

5278ba00a1ea12d1a77a7c0ad232e7a6049b5ceb
Will <doggy.huang@gmail.com>
2020-12-06 10:51:47 +0800

    使用 EF Core Power Tools 快速建立 EF Core 模型與 API 控制器

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ASPNETCore5Demo.Models.Models;
+
+namespace ASPNETCore5Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ContosoUniversityContext _context;
+
+        public CourseController(ContosoUniversityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Course
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Course>>> GetCourse()
+        {
+            return await _context.Course.ToListAsync();
+        }
+
+        // GET: api/Course/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Course>> GetCourse(int id)
+        {
+            var course = await _context.Course.FindAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return course;
+        }
+
+        // PUT: api/Course/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCourse(int id, Course course)
+        {
+            if (id != course.CourseId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(course).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Course
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Course>> PostCourse(Course course)
+        {
+            _context.Course.Add(course);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCourse", new { id = course.CourseId }, course);
+        }
+
+        // DELETE: api/Course/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse(int id)
+        {
+            var course = await _context.Course.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            _context.Course.Remove(course);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CourseExists(int id)
+        {
+            return _context.Course.Any(e => e.CourseId == id);
+        }
+    }
+}

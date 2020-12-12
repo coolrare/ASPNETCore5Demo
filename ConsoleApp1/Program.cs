2fe19ed6015a3f04b6ab4369eb2f8ee57d091da9
Will <doggy.huang@gmail.com>
2020-12-12 13:39:15 +0800

    加入 SwaggerClient 到專案中 (連線服務)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new swaggerClient("https://localhost:44360", new HttpClient());
+
+            var data = await client.CourseAllAsync();
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Title);
+            }
         }
     }
 }

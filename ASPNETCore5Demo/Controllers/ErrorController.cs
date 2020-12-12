1d61f38e59e6f6a8d0dca8b1f0da8b9780590cc3
Will <doggy.huang@gmail.com>
2020-12-12 16:41:03 +0800

    完整的 Error 實作

--- ASPNETCore5Demo/Controllers/ErrorController.cs
+++ ASPNETCore5Demo/Controllers/ErrorController.cs
@@ -1,17 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ASPNETCore5Demo.Controllers
 {
+    [ApiController]
     public class ErrorController : ControllerBase
     {
         [HttpGet("/error")]
-        public IActionResult Index()
+        public ActionResult Error([FromServices] IHostEnvironment webHostEnvironment)
         {
-            return Problem();
+            var feature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            var ex = feature?.Error;
+            var isDev = webHostEnvironment.IsDevelopment();
+            var problemDetails = new ProblemDetails
+            {
+                Status = (int)HttpStatusCode.InternalServerError,
+                Instance = feature?.Path,
+                Title = isDev ? $"{ex.GetType().Name}: {ex.Message}" : "An error occurred.",
+                Detail = isDev ? ex.StackTrace : null,
+            };
+
+            return StatusCode(problemDetails.Status.Value, problemDetails);
         }
     }
+
+
 }

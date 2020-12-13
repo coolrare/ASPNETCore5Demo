825402df2cada562cf87f871a63c4673b353f6be
Will <doggy.huang@gmail.com>
2020-12-13 10:35:53 +0800

    JWT Token-based 驗證: 第二步 - 驗證 Token

--- ASPNETCore5Demo/Startup.cs
+++ ASPNETCore5Demo/Startup.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using ASPNETCore5Demo.Helpers;
 using ASPNETCore5Demo.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace ASPNETCore5Demo
@@ -41,6 +44,34 @@ namespace ASPNETCore5Demo
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ASPNETCore5Demo", Version = "v1" });
             });
+
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.IncludeErrorDetails = true; // Default: true
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // Let "sub" assign to User.Identity.Name
+                        NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+                        // Let "roles" assign to Roles for [Authorized] attributes
+                        RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+
+                        // Validate the Issuer
+                        ValidateIssuer = true,
+                        ValidIssuer = Configuration.GetValue<string>("JwtSettings:Issuer"),
+
+                        ValidateAudience = false,
+                        //ValidAudience = "JwtAuthDemo", // TODO
+
+                        ValidateLifetime = true,
+
+                        ValidateIssuerSigningKey = false,
+
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("JwtSettings:SignKey")))
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,6 +90,7 @@ namespace ASPNETCore5Demo
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

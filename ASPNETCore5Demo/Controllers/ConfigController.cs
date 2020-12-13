7d1eb4858ddff986ae8ba34d1fa7f49ae00c5f0e
Will <doggy.huang@gmail.com>
2020-12-13 15:24:56 +0800

    練習用 ILogger<T> 輸出紀錄

--- ASPNETCore5Demo/Controllers/ConfigController.cs
+++ ASPNETCore5Demo/Controllers/ConfigController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ASPNETCore5Demo.Models;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
 
 namespace ASPNETCore5Demo.Controllers
 {
@@ -13,15 +14,24 @@ namespace ASPNETCore5Demo.Controllers
     public class ConfigController : ControllerBase
     {
         private readonly IOptions<JwtSettings> jwtSettings;
+        private readonly ILogger<ConfigController> logger;
 
-        public ConfigController(IOptions<JwtSettings> jwtSettings)
+        public ConfigController(IOptions<JwtSettings> jwtSettings, ILogger<ConfigController> logger)
         {
             this.jwtSettings = jwtSettings;
+            this.logger = logger;
         }
 
         [HttpGet("")]
         public ActionResult<JwtSettings> GetJwtSettingss()
         {
+            logger.LogTrace("Trace");
+            logger.LogDebug("Debug");
+            logger.LogInformation("Information");
+            logger.LogWarning("Warning");
+            logger.LogError("Error");
+            logger.LogCritical("Critical");
+
             return jwtSettings.Value;
         }
     }
